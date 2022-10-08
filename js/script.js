31ce80b7e448e6e3b0cb098e8b500d4a5fd676d2
Vadym Kolybchuk <99030890+vadimvitamin@users.noreply.github.com>
2022-10-08 13:17:49 +0300

    ewsdxcfvgbnm,

--- js/script.js
+++ js/script.js
@@ -340,6 +340,310 @@
 // ***************************************************
 // Напишіть метод, який очищає масив від усіх непотрібних елементів, таких як false, undefined, порожні рядки, нуль, null
 
-const data = [0, 1, false, 2, undefined, "", 3, null];
-const filter
-console.log(compact(data)); // [1, 2, 3]
+// const data = [0, 1, false, 2, undefined, "", 3, null];
+// const filter
+// console.log(compact(data)); // [1, 2, 3]
+// ****************************8blanded  08.10*************
+// Змініть стиль тексту абзацу за допомогою коду JavaScript
+//
+// <!DOCTYPE html>
+// <html>
+// <head>
+// <meta charset=utf-8 />
+//   <title>JS DOM paragraph style</title>
+// </head>
+// <body>
+// <p id ='text'>JavaScript Exercises</p>
+// <div>
+//   <button id="jsstyle">Style</button>
+// </div>
+// </body>
+// </html>
+//
+// При натисканні на кнопку буде змінено шрифт, розмір шрифту та колір тексту абзацу
+// При наступному натисканні кнопки cтилі будуть знову змінюватись
+// Після кожного кліку будуть підставлятись нові значення із масивів colors, fonts, fontSizes  послідовно
+
+// const colors = ["gold", "green", "yellow", "blue", "black", "brown", "purple"];
+// const fonts = [
+//   "Arial",
+//   "Roboto",
+//   "Open Sans",
+//   "Montserrat",
+//   "Noto Sans",
+//   "Raleway",
+//   "Lato",
+// ];
+// const fontSizes = ["12px", "3rem", "15px", "4em", "24px", "48px", "8em"];
+// const textEl = document.querySelector("#jsstyle");
+// console.log(buttonEl);
+// const buttonEl = addEventListener("click, onChangeStyle");
+
+// function onChangeStyle() {
+//   textEl.style.color = colors[counter];
+//   textEl.style.fontFamily = fonts[counter];
+//   textEl.style.fontSize = fontSizes[counter];
+// }
+// ********************************************
+// Напишіть код JavaScript, щоб кубик рухався, коли настискати стрілки вверх, вниз, вілво, вправо
+
+// <!DOCTYPE html>
+// <html>
+// <head>
+// <meta charset=utf-8 />
+//   <title>Change the content of a cell</title>
+// <style>
+//   .cube {
+//   position: absolute;
+//   width: 50px;
+//   height: 50px;
+//   background: green;
+// }
+// </style>
+// </head>
+// <body>
+//
+// <div class="cube" style="top: 0; left: 0;"></div>
+// </body>
+// </html>
+// ********************************************************************
+// Напишіть функцію JavaScript для додавання рядків до таблиці.
+
+// <!DOCTYPE html>
+// <html>
+// <head><meta charset=utf-8 />
+//   <title>Insert row in a table</title>
+// </head>
+// <body>
+// <table id="sampleTable" border="1">
+//   <tr><td>Row1 cell1</td>
+//     <td>Row1 cell2</td></tr>
+//   <tr><td>Row2 cell1</td>
+//     <td>Row2 cell2</td></tr>
+// </table><br>
+//   <input type="button" value="Insert row">
+// </body>
+// </html>
+// ***********************************************************************
+// Напишіть програму JavaScript для добавлення та видалення елементів зі спадного списку.
+
+// <!DOCTYPE html>
+// <html><head>
+// <meta charset=utf-8 />
+//   <title>Remove items from a dropdown list</title>
+// </head>
+// <body>
+// <form>
+//   <select id="colorSelect">
+//     <option>Red</option>
+//     <option>Green</option>
+//     <option>White</option>
+//     <option>Black</option>
+//   </select>
+//   <input id="removeOption" type="button" value="Select and Remove">
+//     <br/>
+//     <br/>
+//     <br/>
+//     <label htmlFor="add">Add new option</label> <br/>
+//     <input id="add" type="text" placeholder="New option">
+//     <input id="addOption" type="button" value="Add option">
+// </form>
+// </body>
+// </html>
+// ************************************************************************
+// Напишіть програму JavaScript TodoList для добавлення нових завдань у список
+
+// <form>
+//   <label htmlFor="add">Add new task</label> <br/>
+//   <input id="add" type="text" placeholder="New task">
+//     <input id="addTask" type="button" value="Add task">
+//
+//       <ul id="tasks"></ul>
+// </form>
+//
+// ***************************************************************
+// Напишіть програму JavaScript, щоб виділяти жирні слова (<strong>) наступного абзацу, наводячи курсор миші на посилання
+
+// <head>
+//   <meta charSet="UTF-8">
+//     <title>Get And Style All Tags</title>
+// </head>
+// <body>
+// <p>[<a href="#" onMouseOver="highlight()" onMouseOut="return_normal()">On mouse over here bold words of the following
+//   paragraph will be highlighted</a>]</p>
+// <p><strong>We</strong> have just started <strong>this</strong> section for the users (<strong>beginner</strong> to
+//   intermediate) who <strong>want</strong> to work with <strong>various</strong> JavaScript <strong>problems</strong> and
+//   write scripts online to <strong>test</strong> their JavaScript <strong>skill</strong>.</p>
+// </body>
+// **************************************************************************************
+// Напишіть програму на JavaScript, щоб отримати ширину та висоту вікна (щоразу, коли розмір вікна змінюється).
+// Вивдіть ці значення на сторінку
+
+// <!DOCTYPE html>
+// <html>
+// <head>
+// <meta charset=utf-8 />
+//   <title>Window Size : height and width</title>
+// </head>
+// <!-- Resize the window (here output panel) and see the result !-->
+// <body onload="getSize()" onresize="getSize()">
+// <div id="wh">
+//   <!-- Place height and width size here! -->
+// </div>
+// <body>
+// </body>
+// </html>
+// **************************************************************************************************
+// Делегування подій
+// 1. Коли користувач клікає на будь-яку комірку із таблиці, потрібно її зробити активною - добавити клас .active
+// 2. Коли користувач клікає на іншу комірку, вона робиться активною, а всі інші стають неактивними
+// 3. Після перезавантаження сторінки активна комірка зберігається
+
+// <style>
+//   table {
+//   margin: 0 auto;
+//   border-collapse: collapse;
+// }
+//
+//   td {
+//   width: 30px;
+//   height: 30px;
+//   border: 1px solid #ccc;
+//   cursor: pointer;
+// }
+//
+//
+//   .active {
+//   background: #00e871;
+// }
+//
+//
+// </style>
+//
+// <body>
+// <div className="board">
+//   <table>
+//     <tbody>
+//     <tr style="display: block;">
+//       <td className="active"></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr style="display: block;">
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr style="display: block;">
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr style="display: block;">
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr style="display: block;">
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr style="display: block;">
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr style="display: block;">
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr style="display: block;">
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr style="display: block;">
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     <tr style="display: block;">
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//       <td></td>
+//     </tr>
+//     </tbody>
+//   </table>
+// </div>
